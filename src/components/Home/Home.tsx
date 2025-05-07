e0778ea942ad874f2dfed3d3c3a220b11cd2ae99
douglasvcv <douglasvitor1233@gmail.com>
2025-05-06 21:18:42 -0300

    building description of home and menuIcon of Navbar

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,7 @@
 import Img from "../../assets/images/imagem aleatoria para portfolio.avif"
 import styles from "./Home.module.css"
+import doc from "../../assets/docs/Curriculo.pdf"
+import Typerwriter from 'typewriter-effect'
 
 const Home = ()=>{
     return(
@@ -10,8 +12,23 @@ const Home = ()=>{
             </div>
             <div className={styles.description}>
                 <h1>Douglas Vitor</h1>
-                <h2>Full Stack Developer</h2>
-                <a href="./">Cv</a>
+                <div style={{minHeight: '110px', color:'var(--text)'}}>
+                <Typerwriter options={{
+                    strings: [`Full Stack Developer`],
+                    loop:true,
+                    autoStart:true,
+                    cursor:'',
+                    delay:"natural",
+                    wrapperClassName:styles.h1name,
+                    
+                }}/>
+                </div>
+                <div className={styles.links}>
+                <a href={doc} target="blank">
+                CV</a>
+                <a href="https://www.linkedin.com/in/douglas-vitor-vieira/" target="blank">
+                Contact me</a>
+                </div>
             </div>
         </div>
         </>
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -9,6 +9,11 @@ const NavBar = ()=>{
             <li><a href="#" target="blank">Projects</a></li>
             <li><a href="#" target="blank">About me</a></li>
         </ul>
+        <div className={styles.menuWrapper}>
+        <div className={styles.menu}></div>
+        <div className={styles.menu}></div>
+        <div className={styles.menu}></div>
+        </div>
         </div>
         </>
     )
