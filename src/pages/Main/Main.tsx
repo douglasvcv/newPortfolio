3c4a9d51f62b3e96fe3e89a661b3e46d6c665b1f
douglasvcv <douglasvitor1233@gmail.com>
2025-06-09 14:33:01 -0300

    update: reformulating structure of folders

---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,16 @@
+import Img from "../../assets/images/imagem aleatoria para portfolio.avif"
+import Apresentacao from "../../components/Apresentacao/Apresentacao"
+import styles from "./Main.module.css"
+
+
+const Home = ()=>{
+    return(
+        <>
+        <main>
+          <Apresentacao/>  
+        </main>
+        </>
+    )
+}
+
+export default Home
