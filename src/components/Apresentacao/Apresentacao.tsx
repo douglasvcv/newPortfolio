841434630daccbf127a6e8983f3cca9f5d8fe16f
douglasvcv <douglasvitor1233@gmail.com>
2025-06-10 22:31:59 -0300

    feat: building apresentacao component

--- src/components/Apresentacao/Apresentacao.tsx
+++ src/components/Apresentacao/Apresentacao.tsx
@@ -1,19 +1,23 @@
-import doc from "../../assets/docs/Curriculo.pdf"
-import styles from "./Apresentacao.module.css"
 
+import styles from "./Apresentacao.module.css"
+import linkedinImg from "../../assets/images/linkedin.png"
+import githubImg from "../../assets/images/placa-do-github.png"
 const Apresentacao = () =>{
+    
     return (
         <>
         <div className={styles.apres_container}>
             <div className={styles.apres_interno}>
                 <h1>Desenvolvedor FullStack</h1>
-                <p>Olá, eu sou Douglas Vitor. Eu sou um densevolvedor especializado no FullStack </p>
+                <p>Olá, eu sou Douglas Vitor. Eu sou um densevolvedor especializado em FullStack </p>
                 <div className={styles.button_container}>
                     <div className={styles.linkedin_container}>
-                        <button className={styles.linkedin_button}>Linkedin</button>
+                        <a href="" className={styles.linkedin_button}><img src={linkedinImg} alt="" /></a>
+                        
                     </div>
                     <div className={styles.github_container}>
-                        <button className={styles.github_button}>GitHub</button>
+                        <a href="" className={styles.github_button}><img src={githubImg} alt="" /></a>
+                        
                     </div>
                 </div>
             </div>
