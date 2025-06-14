ce4ed2294c2bd3a970285b8d3a5932ca83e7f922
douglasvcv <douglasvitor1233@gmail.com>
2025-06-14 00:41:54 -0300

    feat: add about for Main

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,15 +1,15 @@
-
+import About from "../../components/About/About"
 import Apresentacao from "../../components/Apresentacao/Apresentacao"
 import NavBar from "../../components/NavBar/NavBar"
 import styles from "./Main.module.css"
-
 
 const Main = ()=>{
     return(
         <>
         <NavBar/>
         <main>
-          <Apresentacao/>  
+          <Apresentacao/>
+          <About/>
         </main>
         </>
     )
