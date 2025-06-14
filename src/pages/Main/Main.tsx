4bd63f0c78784bf7af9d4898e955aecec9741387
douglasvcv <douglasvitor1233@gmail.com>
2025-06-14 00:53:12 -0300

    delete: delete main.css

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,7 +1,7 @@
 import About from "../../components/About/About"
 import Apresentacao from "../../components/Apresentacao/Apresentacao"
 import NavBar from "../../components/NavBar/NavBar"
-import styles from "./Main.module.css"
+
 
 const Main = ()=>{
     return(
